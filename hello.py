eb4d6b415e80650ab3be931bf833eff261ad2c32
James <james@thinkfreeagents.com>
2025-08-07 16:52:10 -0400

    Added new message
    Added new message to hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 # James Higbe
-# August 7th, 2025
+# August 6th, 2025
 # COP 1000 - Lab 11 - Simple Python Program
 
+# James Higbe
+# August 7th, 2025
+# Add new message to hello.py
 
 Print("Hello, World!")
+
+Print("Welcome to Github!")
